d753b2311e48345b2cfaef9dc1975e7c3880ff08
Ruturaj Biswal <106529938+ruturaj45@users.noreply.github.com>
2022-06-17 08:14:01 +0100

    Patterns 1st

---
+++ Rectangle_Star_Pattern.c
@@ -0,0 +1,18 @@
+// For Printing rectangle Pattern Using *
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i, j;
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 7; j++)
+        {
+            printf(" *");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
